a690ddcaaa651544ef9c09783642f35eac94869f
lhzd863 <lhzd863@126.com>
2021-07-08 15:17:46 +0800

    run

--- nsqadmin/start.sh
+++ nsqadmin/start.sh
@@ -1,7 +1,7 @@
 nohup ./nsqadmin \
   -graphite-url="" \
-  -http-address="0.0.0.0:4171" \
-  -nsqd-http-address=[] \
+  --http-address="0.0.0.0:4171" \
+  --lookupd-http-address="127.0.0.1:4161" \
   -proxy-graphite=true \
   -template-dir="templates" \
   -use-statsd-prefixes=true \
